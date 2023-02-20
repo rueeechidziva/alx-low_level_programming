b0921c75c3fb0711c73b48b9d90361d54713355b
rueeechidziva <chidzivarutendo86@gmail.com>
2023-02-20 08:18:02 -0800

    intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char a = 'A';           
+int b = 120;
+float c = 123.0f;
+double d = 1222.90;
+char str[] = "Hello";
+printf("\nSize of a: %d", sizeof(a));
+printf("\nSize of b: %d", sizeof(b));
+printf("\nSize of c: %d", sizeof(c));
+printf("\nSize of d: %d", sizeof(d));
+printf("\nSize of str: %d", sizeof(str));
+return (0);
+}
