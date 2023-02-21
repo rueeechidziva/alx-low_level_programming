e0760d372aa24a61182d5b6f96f8193b5cc6466b
rueeechidziva <chidzivarutendo86@gmail.com>
2023-02-20 18:54:48 -0800

    print quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ *
+ * Return: 1 if success.
+ */
+int main(void)
+{
+printf("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+
+return (1);
+}
+
