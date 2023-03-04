1629b83713b91c76e7a6abb9a7f0f2338884f079
rueeechidziva <chidzivarutendo86@gmail.com>
2023-03-04 08:00:07 -0800

    nested loops

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-    char c;
+char c;
 
-    c = 'A';
-    printf("%c: %d\n", c,isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c,isupper(c));
-    return (0);
+c = 'A';
+printf("%c: %d\n", c, isupper(c));
+c = 'a';
+printf("%c: %d\n", c, isupper(c));
+return (0);
 }
