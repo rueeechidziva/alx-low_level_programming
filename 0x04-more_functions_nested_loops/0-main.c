e7966caf1da5ecc77804a4d2783fda675c82ff66
rueeechidziva <chidzivarutendo86@gmail.com>
2023-03-04 07:54:37 -0800

    nested loops

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - check the code.
@@ -11,8 +12,8 @@ int main(void)
     char c;
 
     c = 'A';
-    printf("%c: %d\n", c, isupper(c));
+    printf("%c: %d\n", c,isupper(c));
     c = 'a';
-    printf("%c: %d\n", c, isupper(c));
+    printf("%c: %d\n", c,isupper(c));
     return (0);
 }
